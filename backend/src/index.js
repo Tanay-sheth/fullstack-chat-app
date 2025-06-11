689a57b47c4606fa4d3ea8e7870aa951e62082bf
Tanay-sheth <tanaysheth0108@gmail.com>
2025-06-11 13:15:25 +0530

    try 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,42 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import dotenv from 'dotenv';
-import {connectDB} from './lib/db.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import { app, server } from './lib/socket.js'
-import path from 'path'
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-// const app = express(); deleted as we are now using socket.io
-const PORT = process.env.PORT;
 
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// app.use(express.json());
-app.use(express.json({ limit: "50mb" })); // Adjust limit as needed
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}))
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
